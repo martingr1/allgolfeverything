eafab913fb15ef7d930cb4a27c40257f951a3c19
martingr1 <martingarryrussell@gmail.com>
2019-10-29 10:34:57 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,20 @@
+import os
+from flask import Flask
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = 'all_golf'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
+
+
+@app.route('/')
+def hello():
+    return 'Hello World ...again'
+
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
