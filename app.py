e7a6a35704b7c74dd33bc0eb71d9ae9289d828b4
martingr1 <martingarryrussell@gmail.com>
2019-11-26 12:28:11 +0000

    added dynamic brand selector

--- app.py
+++ app.py
@@ -11,9 +11,15 @@ app.config["MONGO_URI"] = os.environ.get('MONGO_URI', 'mongodb://localhost')
 mongo = PyMongo(app)
 
 @app.route('/')
+@app.route('/add_review')
+def add_review():
+    return render_template("write.html",
+    review_brand=mongo.db.review_brand.find())
+
 @app.route('/get_reviews')
 def get_reviews():
     return render_template("reviews.html", reviews=mongo.db.reviews.find())
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
