ed4761a32461bf1be5097a5d60321f1c4c847ebb
martingr1 <martingarryrussell@gmail.com>
2020-02-06 14:07:35 +0000

    added validation to insert brands function

--- app.py
+++ app.py
@@ -149,19 +149,14 @@ def insert_brand():
             return redirect(url_for('add_review'))
             
             if existing_brand is None:
-                model_doc = {'model_name': request.form.get('model_name')}
-                models.insert_one(model_doc)
-                flash("Model successfully added, please continue with your review.")
+                brand_doc = {'brand_name': request.form.get('brand_name')}
+                brands.insert_one(brand_doc)
+                flash("Brand successfully added, please continue with your review.")
                 return redirect(url_for('add_review'))
             
     else:
         flash("You must be logged in to do this!")
         return render_template("login.html")
-    brands = mongo.db.brands
-    brand_doc = {'brand_name': request.form.get('brand_name')}
-    brands.insert_one(brand_doc)
-    return redirect(url_for('add_review'))
-
 
 @app.route('/insert_model', methods=['POST'])
 def insert_model():
