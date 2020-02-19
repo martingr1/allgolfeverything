ff9e6f0616d9b31e421492ce5feacfba21c83f42
martingr1 <martingarryrussell@gmail.com>
2020-02-19 11:50:50 +0000

    turned off debug

--- app.py
+++ app.py
@@ -330,4 +330,4 @@ def upvoted(review_id):
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
-            debug=True)
+            debug=False)
